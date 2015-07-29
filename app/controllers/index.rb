5918c5bed1e0ddad0f3e942ae81f000c5750195b
agweiler <kheryee@live.com>
2015-07-29 09:52:48 +0800

    complete

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,14 +1,16 @@
 #METHOD 1
 #: 1ST STEP
 get '/' do
+
   erb :index
 end
 #: 2ND STEP
 post '/search_anagrams' do
-  a = Word.pluck(:word)
-  @anagrams = Word.anagrams(params[:user_input],a)
-  puts @anagrams
-  erb :index
+  # a = Word.pluck(:word)
+  # @anagrams = Word.anagrams(params[:user_input],a)
+  # puts @anagrams
+  # erb :index
+  redirect to("/#{params[:user_input]}")
 end
 
 #METHOD 2: TYPE IN LOCALHOST.../CHICKEN(FOR EG.)
